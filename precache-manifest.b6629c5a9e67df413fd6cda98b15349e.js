61b512c33defc6efec77be26a431526a0ad5ce6d
sonhua68 <huawu1314@gmail.com>
2019-08-19 20:58:34 +0900

    Updates

---
+++ precache-manifest.b6629c5a9e67df413fd6cda98b15349e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fe80ea34914bc2b966ea5514cccd29",
+    "url": "/lesstodolist/index.html"
+  },
+  {
+    "revision": "3d232eefb76e23ecb971",
+    "url": "/lesstodolist/static/css/main.1962825d.chunk.css"
+  },
+  {
+    "revision": "7ba93a319573ddeddb6e",
+    "url": "/lesstodolist/static/js/2.41645f8e.chunk.js"
+  },
+  {
+    "revision": "3d232eefb76e23ecb971",
+    "url": "/lesstodolist/static/js/main.8d97c714.chunk.js"
+  },
+  {
+    "revision": "a589a684ac7928709357",
+    "url": "/lesstodolist/static/js/runtime~main.6aeea693.js"
+  }
+]);
